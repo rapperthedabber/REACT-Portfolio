4a741671bbd725fc908ad3f13db22aaa40518969
Jerry <jamesharold1738@gmail.com>
2023-04-20 21:48:39 -0400

    setting up react ghpages

--- portfolio-app/src/components/About.js
+++ portfolio-app/src/components/About.js
@@ -6,12 +6,16 @@ export default function Description({about, setAbout}){
         <div>
         {about ? (
             <>
-              <a class="nav-link" href='#About' onClick ={()=> setAbout(!about)}>About</a>
-              <div>
-                
-              </div>
+              <a class="nav-link" href='#About' onClick ={()=> setAbout(!about)}>Resume</a>
+              <br>
+    </br>
+              <div class="card" style={{width: '18rem'}}>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" id = "linkedIn"><a href="https://www.linkedin.com/in/jerry-dong-002040226/">LinkedIn</a></li>
+  </ul>
+</div>
         </> )
-    : ( <a class="nav-link" href='#About' onClick = {()=> setAbout(!about)}>About</a> )
+    : ( <a class="nav-link" href='#About' onClick = {()=> setAbout(!about)}>Resume</a> )
     }
     </div>)
       
